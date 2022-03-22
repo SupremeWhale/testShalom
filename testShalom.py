2c5a6e5528a2c8f0f77f250a1bb4e910c3b29e16
sthen campana <s10ggoy@yahoo.com>
2022-03-22 10:42:45 -0700

    willy

---
+++ testShalom.py
@@ -0,0 +1,10 @@
+import random
+
+testFam = [1, 2, 3, 4, 5]
+rate = 1
+temp =[]
+for i in range(len(testFam)):
+    x = random.randint(1,100)
+    temp.append(testFam[i])
+    if rate == x:
+        temp.append(x)
